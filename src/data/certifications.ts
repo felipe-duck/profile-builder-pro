1cf8e698bad5729dd2039696a6c1bb89ccbb049e
Felipe Pontes <felipepontes909@gmail.com>
2026-04-05 19:19:51 -0300

    b

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -20,7 +20,8 @@ export const CERTIFICATIONS: Certification[] = [
     status: 'Obtida',
     year: '2026',
     description: 'Certificação de entrada em segurança da informação pela (ISC)², cobrindo conceitos de segurança, controle de acesso, criptografia e resposta a incidentes.',
-    badgeImage: '/badges/CC-ICS2.png',
+    badgeImage: '',
+    //badgeImage: 'import img from '@/assets/nome.jpg'',
   },
   {
     id: 2,
